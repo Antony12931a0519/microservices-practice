655e922071119d8e094cc71a3c0bccac611644be
anthony_sampath <anthony_sampath@II39-DXVRQQ2.mhf2.mhf.mhc>
2019-04-30 14:52:19 +0530

    testing purpose

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -14,5 +14,6 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+		Sysout.
 	}
 }
